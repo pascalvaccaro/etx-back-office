d7855a84472ef5bf71b79021bc309e64a9713b0b
Pascal Vaccaro <pascal.vaccaro@gmail.com>
2022-05-16 10:44:18 +0200

    test: adjust configuration based on an env variable for render.sh

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,13 +1,19 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
+const defaultConfig = require('../../database');
+
+module.exports = ({ env }) => {
+  if (env.bool('STAGING_ENV')) return defaultConfig({ env });
+
+  return {
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
--- config/server.js
+++ config/server.js
@@ -1,10 +1,21 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  cron: {
-    enabled: true,
-  },
-});
+module.exports = ({ env }) => {
+  const serverConfig = {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+    cron: {
+      enabled: true,
+    },
+  };
+
+  if (env.bool('STAGING_ENV')) {
+    serverConfig.url = env('RENDER_EXTERNAL_URL');
+    serverConfig.dirs = {
+      public: '/data/public',
+    };
+  }
+
+  return serverConfig;
+};
