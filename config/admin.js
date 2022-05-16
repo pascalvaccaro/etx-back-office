77bb399afb8fe9270770c35cb6b3f207811a9101
Pascal Vaccaro <pascal.vaccaro@gmail.fr>
2022-05-16 09:59:11 +0200

    chore: prepare files for render deployment

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,10 @@
+const { randomBytes } = require('crypto');
+
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'f33bdfa1eda532257a04efeaefe36247'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT', randomBytes(16).toString('base64')),
   },
 });
