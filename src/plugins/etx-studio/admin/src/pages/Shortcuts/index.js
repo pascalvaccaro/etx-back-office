3cff7b9a9508f23e7554f3d2ca4f6b57b43270e1
Pascal Vaccaro <pascal.vaccaro@gmail.com>
2022-05-19 16:59:08 +0200

    fix: display shortcuts only in the article list page

--- src/plugins/etx-studio/admin/src/pages/Shortcuts/index.js
+++ src/plugins/etx-studio/admin/src/pages/Shortcuts/index.js
@@ -1,9 +1,10 @@
-import React, { useCallback, useReducer } from 'react';
+import React, { useCallback, useMemo, useReducer } from 'react';
 import { useIntl } from 'react-intl';
+import { useLocation } from 'react-router-dom';
 import { Button } from '@strapi/design-system/Button';
 import { IconButton } from '@strapi/design-system/IconButton';
 import DeleteIcon from '@strapi/icons/Cross';
-import { useQueryParams } from '@strapi/helper-plugin';
+import { useQueryParams, useStrapiApp } from '@strapi/helper-plugin';
 import * as queries from './queries';
 import getTrad from '../../utils/getTrad';
 import pluginId from '../../pluginId';
@@ -18,6 +19,8 @@ const shortcuts = [
 ];
 
 const Shortcuts = () => {
+  const location = useLocation();
+  const isArticleListPage = useMemo(() => location === '/content-manager/collectionType/api::article.article', [location]);
   const { formatMessage } = useIntl();
   const [{ query }, setQuery] = useQueryParams();
   const [selected, setSelected] = useReducer((_, value = '') => {
@@ -25,13 +28,13 @@ const Shortcuts = () => {
     return value;
   }, localStorage.getItem(LOCAL_STORAGE_KEY) || '');
 
-  const handleClick = useCallback((shortcut) => async () => {
+  const handleClick = useCallback((shortcut = '') => async () => {
     setSelected(shortcut);
     switch (typeof queries[shortcut]) {
       case 'object':
         setQuery({ ...queries[shortcut], page: 1 });
         break;
-      case 'function': 
+      case 'function':
         setQuery({ ...(await queries[shortcut]()), page: 1 });
         break;
       default:
@@ -39,7 +42,7 @@ const Shortcuts = () => {
     }
   }, [setQuery]);
 
-  return (
+  return isArticleListPage ? (
     <>
       {shortcuts.map((shortcut) => (
         <Button
@@ -52,9 +55,9 @@ const Shortcuts = () => {
           {formatMessage({ id: getTrad(`filters.${shortcut.name}`), defaultMessage: shortcut.name })}
         </Button>
       ))}
-      <IconButton onClick={handleClick('')} key="remove" variant="tertiary" icon={<DeleteIcon />} label={formatMessage({ id: getTrad(`filters.remove`), defaultMessage: "Remove filters" })} />
+      <IconButton onClick={handleClick()} key="remove" variant="tertiary" icon={<DeleteIcon />} label={formatMessage({ id: getTrad('filters.remove'), defaultMessage: 'Remove filters' })} />
     </>
-  );
+  ) : null;
 };
 
 export default Shortcuts;
