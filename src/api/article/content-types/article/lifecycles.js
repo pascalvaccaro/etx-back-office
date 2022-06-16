0b368e12a1a294d3f1284b0f95afcaafc68eb6e4
Pascal Vaccaro <pascal.vaccaro@gmail.com>
2022-06-16 14:37:04 +0200

    feat: prevent a published article from being modified

--- src/api/article/content-types/article/lifecycles.js
+++ src/api/article/content-types/article/lifecycles.js
@@ -1,7 +1,13 @@
+const { ValidationError } = require('@strapi/utils').errors;
+
 module.exports = {
   async beforeUpdate(event) {
     const { data = {}, where = {} } = event.params;
-    if (data.publishedAt && where.id)
-      await strapi.plugin('etx-studio').service('dynamo').sendById(where.id);
+    const article = await strapi.entityService.findOne('api::article.article', where.id, { populate: '*' });
+
+    if (data.publishedAt && !article.publishedAt)
+      await strapi.plugin('etx-studio').service('dynamo').send(article);
+    else if (!data.publishedAt && article.publishedAt)
+      throw new ValidationError('Vous ne pouvez pas éditer un article qui a été publié. Veuillez d\'abord le dépublier.');
   }
 };
